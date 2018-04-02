eacbc0daa5384b4cb9d52698a7730995ed677d06
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2018-04-02 21:39:59 +0000

    Add some content to the mock

--- server/api/middleware.js
+++ server/api/middleware.js
@@ -213,24 +213,11 @@ const dailyEntries = date => async (token) => {
 
 	let endTime = '';
 
-	let isValid = Boolean(startTime);
-	const isDebug = Boolean(process.env.DEBUG);
-	const randomTotalWorked = () => `${parseInt(Math.random() * 5, 10) + 7}:00`;
-	let newTotal = total;
-
-	if (isDebug) {
-		const isBeforeThanToday = !moment(date).isSameOrAfter(moment().startOf('day'));
-		const dayName = moment(date).format('dddd');
-		const isWeekDay = !(dayName === 'Sunday' || dayName === 'Saturday');
-		const isSameWeek = moment(date).isBetween(moment().startOf('week'), moment().subtract(1, 'day'), null, '[)');
-		const randomness = isSameWeek ? parseInt(Math.random() * 10, 10) % 2 === 0 : true;
-		isValid = isBeforeThanToday && isWeekDay && randomness;
-		newTotal = randomTotalWorked();
-	}
+	const isValid = Boolean(startTime);
 
 	if (isValid) {
 		endTime = moment(startTime, 'hh:mm');
-		const totalWorked = moment(newTotal, 'hh:mm');
+		const totalWorked = moment(total, 'hh:mm');
 		const durantion = moment(breakTimeDuration, 'hh:mm');
 		endTime.add({ hours: totalWorked.hours(), minutes: totalWorked.minutes() });
 		endTime.add({ hours: durantion.hours(), minutes: durantion.minutes() });
@@ -246,7 +233,7 @@ const dailyEntries = date => async (token) => {
 		endTime: (isValid && endTime) || '',
 		startBreakTime: (isValid && startBreakTime) || '',
 		endBreakTime: (isValid && endBreakTime) || '',
-		total: (isValid && newTotal) || ''
+		total: (isValid && total) || ''
 	};
 
 	return timeEntry;
